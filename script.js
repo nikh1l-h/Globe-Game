590a0a19beb434d06c53117f897d2812fde1d491
nikh1l.h <nikhilhandley@gmail.com>
2025-10-28 17:50:09 +0000

    all screen change buttons working

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-
 function changePage(currentPageid,nextPageid) {
     const oldPage = document.getElementById(currentPageid);
     oldPage.classList.add('hidden');
 
     const newPage = document.getElementById(nextPageid);
     newPage.classList.remove('hidden');
-}
+};
 
-main_menu = document.getElementById('play-button');
-main_menu.addEventListener("click", () => changePage('menu','tutorial')); /* first test */
+
+document.querySelectorAll('button').forEach(button => { 
+    button.addEventListener('click', event => { 
+        const specificButton = event.target.dataset.buttons; // finds the value of the data-buttons property of the exact element referenced
+        if (specificButton === 'play-from-menu') {changePage('menu','gameplay-screen')};
+        if (specificButton === 'tutorial-from-menu') {changePage('menu','tutorial')};
+        if (specificButton === 'play-from-tutorial') {changePage('tutorial','gameplay-screen')};
+        if (specificButton === 'menu-from-tutorial') {changePage('tutorial','menu')};
+        if (specificButton === 'play-again') {changePage('end-screen','menu')};
+
+    });
+});
 

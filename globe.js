a25e6e87fecd147323a12f906b6e356ba78762d0
nikh1l.h <nikhilhandley@gmail.com>
2025-11-08 22:10:47 +0000

    minor indent change

--- globe.js
+++ globe.js
@@ -9,8 +9,8 @@ const globe = Globe()(document.getElementById('Main-Globe'))
 fetch('https://unpkg.com/world-atlas@2/countries-110m.json')
     .then(res => res.json())
     .then(world => {
-    const countries = topojson.feature(world, world.objects.countries).features; // converts topojson to geojson
-    globe 
+        const countries = topojson.feature(world, world.objects.countries).features; // converts topojson to geojson
+        globe 
         .polygonsData(countries)
         
         .polygonStrokeColor(() => '#ffffff')
@@ -23,7 +23,5 @@ fetch('https://unpkg.com/world-atlas@2/countries-110m.json')
             return 'grey'
             } 
         });
-        
-
     });
 

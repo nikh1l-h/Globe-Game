047982c99d165591cb3ad6fa5bdf30d0d7ac765d
nikh1l.h <nikhilhandley@gmail.com>
2025-11-17 18:11:42 +0000

    converted to OOP approach

--- globe.js
+++ globe.js
@@ -1,36 +1,32 @@
-// Create the globe
-const globe = Globe()(document.getElementById('Main-Globe'))
-    .backgroundColor('#ffffff')
-    .showAtmosphere(false)
-    .globeImageUrl('https://cdn.jsdelivr.net/npm/three-globe/example/img/earth-blue-marble.jpg');
+class Earth {
+    constructor() {
 
+        this.globe = Globe()(container) // New Globe is created
+            .backgroundColor('#ffffff')
+            .showAtmosphere(false)
+            .globeImageUrl('https://cdn.jsdelivr.net/npm/three-globe/example/img/earth-blue-marble.jpg');
+    }
 
+    async init(highlightCountryCode) { // .then and fetch is async therefore we must use init() to load country polygons
+        fetch('globe-properties.json')
+            .then(res => res.json())
+            .then(world => {
+                const countries = topojson.feature(world, world.objects.countries).features; // converts topojson to geojson
+                this.globe 
+                .polygonsData(countries)
+                .polygonStrokeColor(() => '#ffffff')
+                .polygonAltitude(0.01)
+                .polygonSideColor(()=>'#ffffff')
+                .polygonCapColor(choice => {
+                    if (choice.id===highlightCountryCode) {  // 250 = country code for France
+                        return 'red'
+                    } else {
+                    return 'grey'
+                    } 
+                });
+            });
+    } 
+}
 
-fetch('globe-properties.json')
-    .then(res => res.json())
-    .then(world => {
-        const countries = topojson.feature(world, world.objects.countries).features; // converts topojson to geojson
-        globe 
-        .polygonsData(countries)
-        
-        .polygonStrokeColor(() => '#ffffff')
-        .polygonAltitude(0.01)
-        .polygonSideColor(()=>'#ffffff')
-        .polygonCapColor(choice => {
-            if (choice.id==='380') {  // 250 = country code for France
-                return 'red'
-            } else {
-            return 'grey'
-            } 
-        });
-    });
-
-// class Globe {
-//     constructor(this) {
-
-//     }
-
-//     async init(this) {
-
-//     } 
-// }
+const newGlobe = new Earth();
+newGlobe.init('250');
